c205f23b61b32ab9f07150027a9d88c8cfa3113e
bill <billsearle@me.com>
2014-09-13 19:07:35 +1000

    changed css look and added timeout to the for loop

--- js/hue.js
+++ js/hue.js
@@ -4,8 +4,8 @@ var bridgeURL = 'http://192.168.0.2/api'
 // gets info of all lights
 var getAllLights = function() {
 	$.ajax({
-			url: bridgeURL + '/newdeveloper/lights',
-			type: 'get',
+		url: bridgeURL + '/newdeveloper/lights',
+		type: 'get',
 		success: function(response) {
 			lights.all = response;
 			lights.count = Object.keys(lights.all)
@@ -16,35 +16,41 @@ var getAllLights = function() {
 
 var switchOn = function () {
 	for (var i = 0; i < ((lights.count.length)-2); i++) {
-		lights.count[i]
-		$.ajax({
-				url: bridgeURL + '/newdeveloper/lights/'+ lights.count[i]+ '/state',
-				type: 'put',
-				contentType: "text/plain;charset={UTF-8}",
-				dataType: 'json',		
-				data: JSON.stringify({ "on": true }),
-			success: function(response) {
-				// console.log('state true: ');
-				// console.log(response);
-			}
-		});
+		(function(i){
+			setTimeout(function(){
+				lights.count[i]
+				$.ajax({
+					url: bridgeURL + '/newdeveloper/lights/'+ lights.count[i]+ '/state',
+					type: 'put',
+					contentType: "text/plain;charset={UTF-8}",
+					dataType: 'json',		
+					data: JSON.stringify({ "on": true }),
+					// success: function(response) {
+					// 	// console.log('state true: ');
+					// 	// console.log(response);
+				});
+			}, 350 * i);
+		}(i));
 	};
 };
 
 var switchOff = function () {
 	for (var i = 0; i < lights.count.length ; i++) {
-		lights.count[i]
-		$.ajax({
-				url: bridgeURL + '/newdeveloper/lights/'+ lights.count[i]+ '/state',
-				type: 'put',
-				contentType: "text/plain;charset={UTF-8}",
-				dataType: 'json',		
-				data: JSON.stringify({ "on": false }),
-			success: function(response) {
-				// console.log('state true: ');
-				// console.log(response);
-			}
-		});
+		(function(i){
+			setTimeout(function(){
+				lights.count[i]
+				$.ajax({
+					url: bridgeURL + '/newdeveloper/lights/'+ lights.count[i]+ '/state',
+					type: 'put',
+					contentType: "text/plain;charset={UTF-8}",
+					dataType: 'json',		
+					data: JSON.stringify({ "on": false }),
+					// success: function(response) {
+					// 	// console.log('state true: ');
+					// 	// console.log(response);
+				});
+			}, 350 * i);
+		}(i));
 	};
 };
 
